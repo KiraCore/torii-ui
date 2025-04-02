27780fc8ab2f840a912714e4d04c4ddfbdb71ba6
Mykyta Sadchenko <31825654+muknta@users.noreply.github.com>
2025-04-03 01:10:54 +0300

    mock token aliases (they are empty in devnet)

--- lib/domain/services/miro/query_kira_tokens_aliases_service.dart
+++ lib/domain/services/miro/query_kira_tokens_aliases_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:torii_client/data/dto/api_kira/query_kira_tokens_aliases/request/query_kira_tokens_aliases_req.dart';
 import 'package:torii_client/data/dto/api_kira/query_kira_tokens_aliases/response/query_kira_tokens_aliases_resp.dart';
+import 'package:torii_client/data/dto/api_kira/query_kira_tokens_aliases/response/token_alias.dart';
 import 'package:torii_client/data/dto/api_request_model.dart';
 import 'package:torii_client/domain/models/tokens/token_alias_model.dart';
 import 'package:torii_client/domain/models/tokens/token_default_denom_model.dart';
@@ -15,45 +16,91 @@ class QueryKiraTokensAliasesService {
 
   QueryKiraTokensAliasesService(this._apiKiraRepository);
 
-  Future<List<TokenAliasModel>> getTokenAliasModels() async {
-    Uri networkUri = getIt<NetworkModuleBloc>().state.networkUri;
-    Response<dynamic> response = await _apiKiraRepository.fetchQueryKiraTokensAliases<dynamic>(
-      ApiRequestModel<QueryKiraTokensAliasesReq>(
-        networkUri: networkUri,
-        requestData: const QueryKiraTokensAliasesReq(),
+  // Also, dynamic denomination is postponed for an unknown period of time.
+  static const QueryKiraTokensAliasesResp _defaultAliases = QueryKiraTokensAliasesResp(
+    tokenAliases: <TokenAlias>[
+      TokenAlias(
+        decimals: 0,
+        denoms: <String>['KEX'],
+        name: 'KEX',
+        symbol: 'KEX',
+        // TODO(Mykyta): make nullable
+        icon: '',
+        // TODO(Mykyta): make int, and get from api, but in another model
+        amount: '0',
       ),
-    );
+      TokenAlias(
+        decimals: 6,
+        denoms: <String>['ukex'],
+        name: 'ukex',
+        symbol: 'ukex',
+        // TODO(Mykyta): make nullable
+        icon: '',
+        // TODO(Mykyta): make int, and get from api, but in another model
+        amount: '0',
+      ),
+      TokenAlias(
+        decimals: 6,
+        denoms: <String>['v1/ukex'],
+        name: 'v1/ukex',
+        symbol: 'v1/ukex',
+        // TODO(Mykyta): make nullable
+        icon: '',
+        // TODO(Mykyta): make int, and get from api, but in another model
+        amount: '0',
+      ),
+    ],
+    defaultDenom: 'KEX',
+    bech32Prefix: 'kira',
+  );
 
-    try {
-      QueryKiraTokensAliasesResp queryKiraTokensAliasesResp = QueryKiraTokensAliasesResp.fromJson(
-        response.data as Map<String, dynamic>,
-      );
+  Future<List<TokenAliasModel>> getTokenAliasModels() async {
+    QueryKiraTokensAliasesResp queryKiraTokensAliasesResp = _defaultAliases;
+    // Uri networkUri = getIt<NetworkModuleBloc>().state.networkUri;
+    // Response<dynamic> response = await _apiKiraRepository.fetchQueryKiraTokensAliases<dynamic>(
+    //   ApiRequestModel<QueryKiraTokensAliasesReq>(
+    //     networkUri: networkUri,
+    //     requestData: const QueryKiraTokensAliasesReq(),
+    //   ),
+    // );
+
+    // try {
+    //   QueryKiraTokensAliasesResp queryKiraTokensAliasesResp = QueryKiraTokensAliasesResp.fromJson(
+    //     response.data as Map<String, dynamic>,
+    //   );
       return queryKiraTokensAliasesResp.tokenAliases.map(TokenAliasModel.fromDto).toList();
-    } catch (e) {
-      getIt<Logger>().e('QueryKiraTokensAliasesService: Cannot parse getTokenAliasModels() for URI $networkUri $e');
-      rethrow;
-    }
+    // } catch (e) {
+    //   getIt<Logger>().e('QueryKiraTokensAliasesService: Cannot parse getTokenAliasModels() for URI $networkUri $e');
+    //   rethrow;
+    // }
   }
 
   Future<TokenDefaultDenomModel> getTokenDefaultDenomModel(Uri networkUri, {bool forceRequestBool = false}) async {
-    TokenDefaultDenomModel initialTokenDefaultDenomModel = await _getTokenDefaultDenom(
-      networkUri,
-      forceRequestBool: forceRequestBool,
+    QueryKiraTokensAliasesResp queryKiraTokensAliasesResp = _defaultAliases;
+    return TokenDefaultDenomModel(
+      // TODO(Mykyta): useless var ?? valuesFromNetworkExistBool
+      valuesFromNetworkExistBool: true,
+      bech32AddressPrefix: queryKiraTokensAliasesResp.bech32Prefix,
+      defaultTokenAliasModel: await _getAliasByTokenName(queryKiraTokensAliasesResp.defaultDenom),
     );
-    try {
-      TokenAliasModel defaultTokenAliasModel = await _getAliasByTokenName(
-        initialTokenDefaultDenomModel.defaultTokenAliasModel!.name,
-        networkUri: networkUri,
-        forceRequestBool: forceRequestBool,
-      );
-      return TokenDefaultDenomModel(
-        valuesFromNetworkExistBool: true,
-        bech32AddressPrefix: initialTokenDefaultDenomModel.bech32AddressPrefix,
-        defaultTokenAliasModel: defaultTokenAliasModel,
-      );
-    } catch (e) {
-      return initialTokenDefaultDenomModel;
-    }
+    // TokenDefaultDenomModel initialTokenDefaultDenomModel = await _getTokenDefaultDenom(
+    //   networkUri,
+    //   forceRequestBool: forceRequestBool,
+    // );
+    // try {
+    //   TokenAliasModel defaultTokenAliasModel = await _getAliasByTokenName(
+    //     initialTokenDefaultDenomModel.defaultTokenAliasModel!.name,
+    //     networkUri: networkUri,
+    //     forceRequestBool: forceRequestBool,
+    //   );
+    // return TokenDefaultDenomModel(
+    //   valuesFromNetworkExistBool: true,
+    //   bech32AddressPrefix: initialTokenDefaultDenomModel.bech32AddressPrefix,
+    //   defaultTokenAliasModel: defaultTokenAliasModel,
+    // );
+    // } catch (e) {
+    //   return initialTokenDefaultDenomModel;
+    // }
   }
 
   Future<TokenDefaultDenomModel> _getTokenDefaultDenom(Uri networkUri, {bool forceRequestBool = false}) async {
@@ -81,23 +128,30 @@ class QueryKiraTokensAliasesService {
     Uri? networkUri,
     bool forceRequestBool = false,
   }) async {
-    networkUri ??= getIt<NetworkModuleBloc>().state.networkUri;
-    Response<dynamic> response = await _apiKiraRepository.fetchQueryKiraTokensAliases<dynamic>(
-      ApiRequestModel<QueryKiraTokensAliasesReq>(
-        networkUri: networkUri,
-        requestData: QueryKiraTokensAliasesReq(tokens: <String>[tokenName]),
-        forceRequestBool: forceRequestBool,
+    QueryKiraTokensAliasesResp queryKiraTokensAliasesResp = _defaultAliases;
+    return TokenAliasModel.fromDto(
+      queryKiraTokensAliasesResp.tokenAliases.firstWhere(
+        (TokenAlias alias) => alias.name == tokenName,
+        orElse: () => queryKiraTokensAliasesResp.tokenAliases.first,
       ),
     );
+    // networkUri ??= getIt<NetworkModuleBloc>().state.networkUri;
+    // Response<dynamic> response = await _apiKiraRepository.fetchQueryKiraTokensAliases<dynamic>(
+    //   ApiRequestModel<QueryKiraTokensAliasesReq>(
+    //     networkUri: networkUri,
+    //     requestData: QueryKiraTokensAliasesReq(tokens: <String>[tokenName]),
+    //     forceRequestBool: forceRequestBool,
+    //   ),
+    // );
 
-    try {
-      QueryKiraTokensAliasesResp queryKiraTokensAliasesResp = QueryKiraTokensAliasesResp.fromJson(
-        response.data as Map<String, dynamic>,
-      );
-      return TokenAliasModel.fromDto(queryKiraTokensAliasesResp.tokenAliases.first);
-    } catch (e) {
-      getIt<Logger>().e('QueryKiraTokensAliasesService: Cannot parse getAliasByTokenName() for URI $networkUri $e');
-      rethrow;
-    }
+    // try {
+    //   QueryKiraTokensAliasesResp queryKiraTokensAliasesResp = QueryKiraTokensAliasesResp.fromJson(
+    //     response.data as Map<String, dynamic>,
+    //   );
+    //   return TokenAliasModel.fromDto(queryKiraTokensAliasesResp.tokenAliases.first);
+    // } catch (e) {
+    //   getIt<Logger>().e('QueryKiraTokensAliasesService: Cannot parse getAliasByTokenName() for URI $networkUri $e');
+    //   rethrow;
+    // }
   }
 }
